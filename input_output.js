14ee338c9f1578a1d8fcf9341613ce37e8a7d743
Calderaro <natacsouza12@gmail.com>
2026-01-19 21:14:31 -0300

    Add files via upload

---
+++ input_output.js
@@ -0,0 +1,15 @@
+let nomeUsuario = "Caldera";
+//alert: popup web
+console.log("Visão, "+nomeUsuario)
+
+//Comentrio 1 linha
+/* Comentarios
+com varias linhas */
+
+//var,let e cont
+/*VAriaveis são nomes simbolicos que apontam para 
+espaços ma memoria do computador, usados para armazenar dados
+let: declaro variavel limitando o escopo no bloco, instruçao. Variavel limitada 
+var: Variavel global, pode ser acessada de qualquer lugar do programa
+const: contante - algo fixo. Ex.: Pi
+*/
